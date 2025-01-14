1013e0b93758c23b9b70f1e8a4aa5a6ba59d6a0c
Nihal N <112004307+nihalshx@users.noreply.github.com>
2025-01-14 12:53:39 +0530

    Delete Sum_of_n_numbers.c

--- Sum_of_n_numbers.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-	int range, sum = 0;
-	printf("Enter the range of numbers you want to get sum of\n");
-	scanf("%d",&range);
-	for(int i = 1; i <= range; i++)
-		sum = sum + i;
-	printf("Sum of %d numbers  is %d\n",range, sum);
-}
